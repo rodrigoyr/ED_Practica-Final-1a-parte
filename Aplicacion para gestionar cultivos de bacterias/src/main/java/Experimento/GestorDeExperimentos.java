eca9d7e3dca7b87a6551c8ee6ff34fec4c82e6ca
rodrigoyr <ryeperub@myuax.com>
2024-05-03 17:02:04 +0200

    commit

--- Aplicacion para gestionar cultivos de bacterias/src/main/java/Experimento/GestorDeExperimentos.java
+++ Aplicacion para gestionar cultivos de bacterias/src/main/java/Experimento/GestorDeExperimentos.java
@@ -1,1 +1,32 @@
+package Experimento;
 
+import Archivo.GestorDeArchivos;
+
+public class GestorDeExperimentos {
+    private Experimento experimentoActual;
+    private GestorDeArchivos gestorDeArchivos;
+
+    public GestorDeExperimentos() {
+        this.gestorDeArchivos = new GestorDeArchivos();
+    }
+
+    public void crearNuevoExperimento() {
+        this.experimentoActual = new Experimento();
+    }
+
+    public void abrirExperimento(String rutaArchivo) {
+        this.experimentoActual = this.gestorDeArchivos.abrirArchivo(rutaArchivo);
+    }
+
+    public void guardarExperimento(String rutaArchivo) {
+        this.gestorDeArchivos.guardarArchivo(rutaArchivo, this.experimentoActual);
+    }
+
+    public void guardarExperimentoComo(String rutaArchivo) {
+        this.gestorDeArchivos.guardarArchivo(rutaArchivo, this.experimentoActual);
+    }
+
+    public Experimento getExperimentoActual() {
+        return this.experimentoActual;
+    }
+}
